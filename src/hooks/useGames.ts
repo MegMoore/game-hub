deca0b444bea88f9ebd4e2d36a91b4b2e2a11074
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-01-09 13:16:21 -0500

    .

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponce {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponce>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+      
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
